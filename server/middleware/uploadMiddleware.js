48a9acdc8e549a80ddceb71682dece0d7c179293
impalfoodscontact-pixel <impalfoodscontact@gmail.com>
2026-07-11 13:19:43 +0530

    Update uploadMiddleware.js

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 20 * 1024 * 1024 }, // 5MB
   fileFilter: (req, file, cb) => {
     const allowed = /jpeg|jpg|png|webp/;
     const ext = allowed.test(path.extname(file.originalname).toLowerCase());
